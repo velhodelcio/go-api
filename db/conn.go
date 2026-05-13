a689dd400c410958578a05fd4a33268001553ab1
velhodelcio <delcimarioalvesdev@gmail.com>
2026-05-13 16:46:58 -0300

    feat: Add database connection functionality for PostgreSQL

---
+++ db/conn.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "go_db"
+	port     = 5432
+	user     = "postgres"
+	password = "12345"
+	dbname   = "postgres"
+)
+
+func ConenctDB() (*sql.DB, error) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to " + dbname)
+
+	return db, nil
+}
