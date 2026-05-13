3e2818672876a1320301094ade2f18bdef716ecd
velhodelcio <delcimarioalvesdev@gmail.com>
2026-05-13 19:13:08 -0300

    feat: Add DELETE endpoint for removing a product by ID

--- cmd/main.go
+++ cmd/main.go
@@ -33,6 +33,7 @@ func main() {
 	server.GET("/products", ProductController.GetProducts)
 	server.POST("/product", ProductController.CreateProduct)
 	server.GET("/product/:productId", ProductController.GetProductById)
+	server.DELETE("/product/:productId", ProductController.DeleteProductById)
 
 	server.Run(":8000")
 }
