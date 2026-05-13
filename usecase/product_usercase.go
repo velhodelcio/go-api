ade6cb125ab0cf112c6b9d6b27e7607b032fca09
velhodelcio <delcimarioalvesdev@gmail.com>
2026-05-13 16:47:36 -0300

    feat: Implement ProductUsecase with methods for creating and retrieving products

---
+++ usecase/product_usercase.go
@@ -0,0 +1,41 @@
+package usecase
+
+import (
+	"go-api/model"
+	"go-api/repository"
+)
+
+type ProductUsecase struct {
+	respository repository.ProductRepository
+}
+
+func NewProductUseCase(repo repository.ProductRepository) ProductUsecase {
+	return ProductUsecase{
+		respository: repo,
+	}
+}
+
+func (pu *ProductUsecase) GetProducts() ([]model.Product, error) {
+	return pu.respository.GetProducts()
+}
+
+func (pu *ProductUsecase) CreateProduct(product model.Product) (model.Product, error) {
+	productId, err := pu.respository.CreateProduct(product)
+	if err != nil {
+		return model.Product{}, err
+	}
+
+	product.ID = productId
+
+	return product, nil
+}
+
+func (pu *ProductUsecase) GetProductById(id_product int) (*model.Product, error) {
+	product, err := pu.respository.GetProductById(id_product)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
