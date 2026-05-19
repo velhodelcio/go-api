76c266954dbe077188b582060cf9f45d454f6841
velhodelcio <delcimarioalvesdev@gmail.com>
2026-05-19 17:20:33 -0300

    feat: Implement UpdateProduct method with error handling and validation

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -120,3 +120,45 @@ func (p *productController) DeleteProductById(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, product)
 }
+
+func (p *productController) UpdateProduct(ctx *gin.Context) {
+	var product model.Product
+	err := ctx.BindJSON(&product)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, err)
+		return
+	}
+	id := product.ID
+
+	if id == 0 {
+		response := model.Reponse{
+			Message: "Id do produto nao pode ser nulo",
+		}
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	//Pesquisa pelo id do produto informado para verificar se ele existe no banco de dados antes de tentar atualizar
+	productInDB, err := p.productUsecase.GetProductById(id)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	if productInDB == nil {
+		response := model.Reponse{
+			Message: "Produto nao foi encontrado",
+		}
+		ctx.JSON(http.StatusNotFound, response)
+		return
+	}
+
+	updatedProductProduct, err := p.productUsecase.UpdateProduct(product)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, updatedProductProduct)
+}
